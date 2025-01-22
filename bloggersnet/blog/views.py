47b09cf3403844c2dc9b13002470238b12ba0556
John_Lamy <johnclamy@gmail.com>
2025-01-22 21:06:50 +0000

    setup the blog post views. One for the list and another for the detailed page

--- bloggersnet/blog/views.py
+++ bloggersnet/blog/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.http import Http404
+from .models import Post
 
-# Create your views here.
+
+def post_list(request):
+  posts = Post.published.all()
+  return render(request, 'blog/post/list.html', { 'posts': posts })
+
+
+def post_detail(request, id):
+  try:
+    post = Post.publish.get(id=id)
+  except Post.DoesNotExist:
+    raise Http404('No post found.')
+  return(request, 'blog/post/detail.html', {'post': post })
