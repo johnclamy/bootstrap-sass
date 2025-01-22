38665bc1a00fc65dfa12d40d3b2bc90002bf4861
John_Lamy <johnclamy@gmail.com>
2025-01-22 21:33:51 +0000

    get_object_or_404() shortcut should be used instead to retrieve the desired single post

--- bloggersnet/blog/views.py
+++ bloggersnet/blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from django.http import Http404
 from .models import Post
 
@@ -9,8 +9,5 @@ def post_list(request):
 
 
 def post_detail(request, id):
-  try:
-    post = Post.published.get(id=id)
-  except Post.DoesNotExist:
-    raise Http404('No post found.')
-  return(request, 'blog/post/detail.html', {'post': post })
+  post = get_object_or_404(Post, id=id, status=Post.Status.PUBLISHED)
+  return render(request, 'blog/post/detail.html', { 'post': post })
